f19bac413cbf4f5a08e9a2ef9351a27df00c3f2c
@Mikist <mikeochango3@gmail.com>
2024-01-16 15:53:24 +0300

    create route test api

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 import express from "express"
 import mongoose  from "mongoose"
 import dotenv from 'dotenv'
+import cors from 'cors'
+import userRoutes from './routes/user.routes.js'
+
 dotenv.config()
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/user', userRoutes )
+
+
+
+
 
 app.listen(5000,()=> {
     console.log('Server running on port 5000')})
